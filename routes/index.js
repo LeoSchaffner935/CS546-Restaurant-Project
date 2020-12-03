b1e2d7c348a17991a25823003325722844d81f06
Leo Schaffner <lschaffn@stevens.edu>
2020-12-03 16:24:52 -0500

    Added login route

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const userRoutes = require('./users');
 const restaurantRoutes = require('./restaurants');
 const serachRoutes = require('./search');
 const privateRoutes = require('./private');
+const loginRoutes = require('./login');
 
 const constructorMethod = (app) => {
   app.use('/users', userRoutes);
   app.use('/restaurants', restaurantRoutes);
   app.use('/search', serachRoutes);
   app.use('/private', privateRoutes);
+  app.use('/login', loginRoutes);
 
   app.use('*', (req, res) => {
     res.sendStatus(404);

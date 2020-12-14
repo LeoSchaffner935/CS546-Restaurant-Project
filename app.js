56a77d017647f8aa247e8be7fa811a417e044541
Rgray11 <32401284+Rgray11@users.noreply.github.com>
2020-12-14 04:09:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -7,9 +7,31 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    name:"AuthCookie",
+    secret:"secretcookiedontbiteit",
+    resave:false,
+    saveUninitialized:true
+    })
+);
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+let authorizeUsr = function(req,res,next){
+    if(req.originalUrl == "/user"){
+        if(req.session.userLogged){
+            next();
+        }
+        else{
+            res.status(403).render("templates/Login");
+        }
+    }
+    else{
+        next();
+    }
+}
+app.use(authorizeUsr);
 configRoutes(app);
 app.listen(3000, () => {
   console.log("Server On!");
